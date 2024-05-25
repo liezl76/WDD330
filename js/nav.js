fdc018efc7b9137348553645bb3bc36c578bf16e
liezl76 <liezl1106@gmail.com>
2024-05-25 23:31:30 +0800

    edit index.html and css

--- js/nav.js
+++
@@ -1,5 +0,0 @@
-let menuToggle = document.getElementById("bars");
-let menu = document.querySelector("nav span ul");
-menuToggle.addEventListener("click", function () {
-    menu.classList.toggle("show");
-});
