ea1c884c014713e5bedbaf0f988f13048d3907ec
liezl76 <liezl1106@gmail.com>
2024-06-01 09:56:03 +0800

    add tournament.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const url = 'https://volleyball-devs.p.rapidapi.com/media-players?lang=en';
+    const url = 'https://volleyball-devs.p.rapidapi.com/media-leagues?limit=50&offset=0&league_id=eq.3024&lang=en';
     const options = {
         method: 'GET',
         headers: {
@@ -11,19 +11,41 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        
-        // Assuming the result contains an array of media items
-        if (result && result.length > 0) {
-            const video = result[0]; // Taking the first video in the result for simplicity
-            
-            document.getElementById('title').textContent = video.title;
-            document.getElementById('subtitle').textContent = video.subtitle;
-            document.getElementById('thumbnail').src = video.thumbnail_url;
-            document.getElementById('video-link').href = video.url;
-            document.getElementById('date-published').textContent = new Date(video.date_published).toLocaleDateString();
-        } else {
-            console.log('No video found for the specified player.');
-        }
+
+        const videosContainer = document.getElementById('videos-container');
+
+        // Filter to get only women's volleyball games
+        const womensGames = result.filter(game => game.gender === 'women');
+
+        womensGames.forEach(video => {
+            const videoCard = document.createElement('div');
+            videoCard.className = 'video-card';
+
+            const videoTitle = document.createElement('h1');
+            videoTitle.textContent = video.title;
+
+            const videoSubtitle = document.createElement('h2');
+            videoSubtitle.textContent = video.subtitle;
+
+            const videoThumbnail = document.createElement('img');
+            videoThumbnail.src = video.thumbnail_url;
+            videoThumbnail.alt = `Thumbnail of ${video.title}`;
+
+            const videoLink = document.createElement('a');
+            videoLink.href = video.url;
+            videoLink.target = '_blank';
+            videoLink.appendChild(videoThumbnail);
+
+            const videoDate = document.createElement('p');
+            videoDate.innerHTML = `Published Date: <span>${new Date(video.date_published).toLocaleDateString()}</span>`;
+
+            videoCard.appendChild(videoTitle);
+            videoCard.appendChild(videoSubtitle);
+            videoCard.appendChild(videoLink);
+            videoCard.appendChild(videoDate);
+
+            videosContainer.appendChild(videoCard);
+        });
     } catch (error) {
         console.error(error);
     }
